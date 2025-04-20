49f64de3772f76b4fb96b39849d0675f2f18b1bf
JCEducadorFisico <jonesciarlini@gmail.com>
2025-04-19 22:11:29 -0300

    Add files via upload

---
+++ JcfitproPlatformUi.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+const App = () => <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
+  <h1>JC FIT PRO</h1>
+  <p>Bem-vindo à plataforma!</p>
+</div>;
+export default App;
