bd6641c4086c34b9fdc4aac477b0c7dc4ff2c23d
JCEducadorFisico <jonesciarlini@gmail.com>
2025-04-22 23:22:34 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-'use client';
-import React from 'react';
-import App from '../JcfitproPlatformUi';
-export default function Page() {
+"use client";
+
+import App from "../JcfitproPlatformUi";
+
+export default function Home() {
   return <App />;
 }
