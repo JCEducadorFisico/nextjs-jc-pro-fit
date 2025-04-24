5ebec61258cecfb3eae4b0c09150e40da3049b68
JCEducadorFisico <jonesciarlini@gmail.com>
2025-04-23 22:39:26 -0300

    Update ajustesBiomecanicos.ts

--- config/ajustesBiomecanicos.ts
+++ config/ajustesBiomecanicos.ts
@@ -1,6 +1,5 @@
-
+// config/ajustesBiomecanicos.ts
 const ajustesBiomecanicos = [
-  { zona: 'lombar', obs: 'Fortalecer região lombar com pranchas' },
-  { zona: 'joelho', obs: 'Evitar exercícios de alto impacto' }
+  { zona: 'ombro', obs: 'Mobilidade reduzida' },
+  { zona: 'joelho', obs: 'Alinhamento a corrigir' },
 ];
-export default ajustesBiomecanicos;
