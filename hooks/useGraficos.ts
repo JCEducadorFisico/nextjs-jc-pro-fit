2cf678f09bcfdacd01b427420f40c1f9f1c56641
JCEducadorFisico <jonesciarlini@gmail.com>
2025-04-23 22:38:54 -0300

    Update useGraficos.ts

--- hooks/useGraficos.ts
+++ hooks/useGraficos.ts
@@ -1,4 +1,7 @@
-
-export function useGraficos(nome) {
-  return [{ dia: 'Seg', valor: 50 }, { dia: 'Qua', valor: 60 }];
-}
+// hooks/useGraficos.ts
+export const useGraficos = (aluno: string) => {
+  return {
+    dias: ['20/04', '22/04'],
+    pse: [6, 7],
+  };
+};
